1fad0d4dc25eb55a4a28c818f0722431f42cdce3
Duminda Wanninayake <deemind@gmail.com>
2019-06-04 23:48:30 +0530

    Added GraphQL mutations

--- dao/users.dao.js
+++ dao/users.dao.js
@@ -2,25 +2,69 @@ let users;
 
 export default class UsersDAO {
 
-    static async injectDB(db) {
-     
+  static async injectDB(db) {
+
     try {
-        users = await db.collection("users");
-      } catch (e) {
-        console.error(`Unable to establish collection handles in userDAO: ${e}`)
+      users = await db.collection("users");
+    } catch (e) {
+      console.error(`Unable to establish collection handles in userDAO: ${e}`)
+    }
+  }
+
+  static async getAllUsers() {
+    return await users.find({}/*, { projection: { 'username' : 1, 'nic' : 1, '_id': 0 }}*/
+    ).toArray();
+  }
+
+  static async getUserByUsername(username) {
+    return await users.findOne({ 'username': username });
+  }
+
+  static async addNewUser(userObj) {
+    return await users.insertOne(userObj.user);
+  }
+
+  /*
+{ lastErrorObject: { n: 1, updatedExisting: true },
+  value:
+   { _id: 5cf6a37be972dd1e4fa62c44,
+     username: 'dum7',
+     firstname: 'Duminda2',
+     lastname: 'Wanninayake',
+     password: 'abc123',
+     nic: '870750986V',
+     deviceid: '1234567',
+     email: 'deemind@gmail.com',
+     tp: '0779906999',
+     location: { type: 'Point', coordinates: [Array] },
+     currentaddr: { district: 'Jaffna', dsdivision: 'Periamia' },
+     verified: false,
+     blackListed: false },
+  ok: 1,
+  operationTime:
+   Timestamp { _bsontype: 'Timestamp', low_: 3, high_: 1559669621 },
+  '$clusterTime':
+   { clusterTime:
+      Timestamp { _bsontype: 'Timestamp', low_: 3, high_: 1559669621 },
+     signature: { hash: [Binary], keyId: [Long] } } }
+
+  */
+
+  static async updateUser(username, firstname, lastname) {
+    return await users.findOneAndUpdate({
+      username: username
+    }, {
+      $set: {
+        firstname: firstname,
+        lastname: lastname
       }
-    }
+      })
+  }
 
-    static async getAllUsers(){
-
-        return await users.find({}/*, { projection: { 'username' : 1, 'nic' : 1, '_id': 0 }}*/
-        ).toArray();
-    }
-
-    static async getUserByUsername(username){
-      console.log(username);
-      
-      return await users.findOne({'username': username});
-    }
+  static async deleteUser(username) {
+    return await users.findOneAndDelete({
+      username: username
+    })
+  }
 
 }
--- index.js
+++ index.js
@@ -7,20 +7,83 @@ var connection = new Connection().getInstance();
 let users;
 
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
+  # User Output Object.
   type User {
+    id: ID!
     username: String!
+    firstname:String!
+    lastname: String!
+    password: String!
     nic: String!
+    deviceid: String!
+    email: String!
+    tp: String!
+    location: Location!
+    currentaddr: Address!
+    verified: Boolean!
+    blackListed: Boolean!
   }
 
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
+  type Location {
+    type: String!
+    coordinates: [Float!]
+  }
+
+  type Address {
+    district: String!
+    dsdivision: String!
+  }
+
+  # User Input Object.
+
+  input UserInput {
+    username: String!
+    firstname:String!
+    lastname: String!
+    password: String!
+    nic: String!
+    deviceid: String!
+    email: String!
+    tp: String!
+    location: LocationInput!
+    currentaddr: AddressInput!
+    verified: Boolean!
+    blackListed: Boolean!
+  }
+
+  input LocationInput {
+    type: String!
+    coordinates: [Float!]
+  }
+
+  input AddressInput {
+    district: String!
+    dsdivision: String!
+  }
+
+  type Response {
+    acknowledged: String!
+    insertedId: ID!
+  }
+
+
+ 
+  # User related queries
   type Query {
     users: [User],
     user(username: String!): User
   }
+
+  # Adding Users/Updating and deleting
+
+  type Mutation {
+    adduser(user: UserInput!): Response!,
+    updateuser(username: String!, firstname: String!, lastname: String!): User,
+    deleteuser(username: String!): User
+  }
+
+
+
 `;
 
 const resolvers = {
@@ -28,12 +91,26 @@ const resolvers = {
     users: async () =>  {
       return await UsersDAO.getAllUsers();
     },
-    user: async (username)=>{
-      console.log(username);
-      
-      return await UsersDAO.getUserByUsername(username);
+    user: async (obj, args, context, info)=>{
+      return await UsersDAO.getUserByUsername(args.username);
     },
   },
+  Mutation:{
+    adduser: async (obj, args, context, info) =>{
+      return await UsersDAO.addNewUser(args);
+    },
+    updateuser: async(obj, args, context, info) => {
+      return await UsersDAO.updateUser(args.username, args.firstname, args.lastname).then(data=>{        
+        return data.value
+      });
+    },
+    deleteuser: async(obj, args, context, info) => {
+      return await UsersDAO.deleteUser(args.username).then(data=>{
+        return data.value
+        
+      })
+    }
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
